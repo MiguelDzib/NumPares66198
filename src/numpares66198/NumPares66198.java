22d9a3571b5838849a80cfd82a7dddc3f56fa5e1
galbe <galbe@192.168.1.68>
2021-02-25 10:45:46 -0600

    Numeros pares

---
+++ src/numpares66198/NumPares66198.java
@@ -0,0 +1,22 @@
+
+package numpares66198;
+
+public class NumPares66198 {
+
+
+    public static void main(String[] args) {
+        
+    int i =0;
+    while (i<94){
+        if(i%2==0){
+          System.out.println( "numero par: " + i);
+          }
+     i++;  
+    }
+    
+    
+    
+        
+    }
+    
+}
